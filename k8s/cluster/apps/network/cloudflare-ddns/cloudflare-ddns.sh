14f1158c5947a8e91c4ca2f417babda6743f6fff
Chris Burnham <16470300+christopherburnham@users.noreply.github.com>
2022-06-10 13:55:27 -0500

    fix(cluster): update cloudflare-ddns vars

--- k8s/cluster/apps/network/cloudflare-ddns/cloudflare-ddns.sh
+++ k8s/cluster/apps/network/cloudflare-ddns/cloudflare-ddns.sh
@@ -5,16 +5,16 @@ set -o errexit
 
 current_ipv4="$(curl -s https://ipv4.icanhazip.com/)"
 zone_id=$(curl -s -X GET \
-    "https://api.cloudflare.com/client/v4/zones?name=${SECRET_CLOUDFLARE_DDNS_RECORD_NAME#*.}&status=active" \
-    -H "X-Auth-Email: ${SECRET_CLOUDFLARE_EMAIL}" \
-    -H "X-Auth-Key: ${SECRET_CLOUDFLARE_GLOBAL_API_KEY}" \
+    "https://api.cloudflare.com/client/v4/zones?name=${CLOUDFLARE_RECORD_NAME#*.}&status=active" \
+    -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" \
+    -H "X-Auth-Key: ${CLOUDFLARE_APIKEY}" \
     -H "Content-Type: application/json" \
         | jq --raw-output ".result[0] | .id"
 )
 record_ipv4=$(curl -s -X GET \
-    "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records?name=${SECRET_CLOUDFLARE_DDNS_RECORD_NAME}&type=A" \
-    -H "X-Auth-Email: ${SECRET_CLOUDFLARE_EMAIL}" \
-    -H "X-Auth-Key: ${SECRET_CLOUDFLARE_GLOBAL_API_KEY}" \
+    "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records?name=${CLOUDFLARE_RECORD_NAME}&type=A" \
+    -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" \
+    -H "X-Auth-Key: ${CLOUDFLARE_APIKEY}" \
     -H "Content-Type: application/json" \
 )
 old_ip4=$(echo "$record_ipv4" | jq --raw-output '.result[0] | .content')
@@ -25,10 +25,10 @@ fi
 record_ipv4_identifier="$(echo "$record_ipv4" | jq --raw-output '.result[0] | .id')"
 update_ipv4=$(curl -s -X PUT \
     "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records/${record_ipv4_identifier}" \
-    -H "X-Auth-Email: ${SECRET_CLOUDFLARE_EMAIL}" \
-    -H "X-Auth-Key: ${SECRET_CLOUDFLARE_GLOBAL_API_KEY}" \
+    -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" \
+    -H "X-Auth-Key: ${CLOUDFLARE_APIKEY}" \
     -H "Content-Type: application/json" \
-    --data "{\"id\":\"${zone_id}\",\"type\":\"A\",\"proxied\":true,\"name\":\"${SECRET_CLOUDFLARE_DDNS_RECORD_NAME}\",\"content\":\"${current_ipv4}\"}" \
+    --data "{\"id\":\"${zone_id}\",\"type\":\"A\",\"proxied\":true,\"name\":\"${CLOUDFLARE_RECORD_NAME}\",\"content\":\"${current_ipv4}\"}" \
 )
 if [[ "$(echo "$update_ipv4" | jq --raw-output '.success')" == "true" ]]; then
     printf "%s - Success - IP Address '%s' has been updated" "$(date -u)" "${current_ipv4}"
