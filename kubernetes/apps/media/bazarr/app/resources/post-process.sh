712bce2b1ad146835cb2cff355cfa4ebed1f438f
Chris Burnham <16470300+blthazr@users.noreply.github.com>
2024-09-13 17:14:31 -0500

    fix: update url

--- kubernetes/apps/media/bazarr/app/resources/post-process.sh
+++ kubernetes/apps/media/bazarr/app/resources/post-process.sh
@@ -19,5 +19,5 @@ if [[ -n "$section" ]]; then
         --data-urlencode "path=$(dirname "$1")" \
         --data-urlencode "X-Plex-Token=${PLEX_API_TOKEN}" \
         --no-progress-meter \
-            "http://plex.default.svc.cluster.local:32400/library/sections/${section}/refresh"
+            "http://plex.media.svc.cluster.local:32400/library/sections/${section}/refresh"
 fi
